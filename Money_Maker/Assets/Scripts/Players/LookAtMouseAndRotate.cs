a0129d7873fceb2c12d40d851615d73c27c9b613
KrSergei <34679066+KrSergei@users.noreply.github.com>
2021-05-21 23:25:47 +0300

    Cheking error
    Исправление поворота персонажа

--- Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
+++ Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
@@ -4,16 +4,20 @@ public class LookAtMouseAndRotate : MonoBehaviour
 {
     public float offset = 5f;   //Угол поворота игрока
 
-    private Vector3 currentMousePos;    //Текущая позиция курсора мышки
+    //private Vector3 currentMousePos;    //Текущая позиция курсора мышки
+    private Vector2 currentMousePos;    //Текущая позиция курсора мышки
+    private Vector2 playerPos;
 
     void Update()
     {
+        #region working var
         //Получение значения курсора экрана в мировых координатах
-        currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         //Вычисление угла поворота по оси Z
         float rotateZ = Mathf.Atan2(currentMousePos.y, currentMousePos.x) * Mathf.Rad2Deg;
         //Поворот на заданный угол по оси Z с добавлением укла поворота
         transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
+        #endregion
     }
 
     /// <summary>
