13a714335980c1a31550089c262e10ec83c0e8f8
KrSergei <34679066+KrSergei@users.noreply.github.com>
2021-05-21 14:48:45 +0300

    AddAnimation
    Добавление анимации передвижения

--- Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
+++ Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtMouseAndRotate : MonoBehaviour
 {
-    public float offset = 5f;
+    public float offset = 5f;   //Угол поворота игрока
 
     void Update()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //Получение значения курсора экрана в мировых координатах
+        Vector3 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Вычисление угла поворота по оси Z
+        float rotateZ = Mathf.Atan2(currentMousePos.y, currentMousePos.x) * Mathf.Rad2Deg;
+        //Поворот на заданный угол по оси Z с добавлением укла поворота
         transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
     }
 }
