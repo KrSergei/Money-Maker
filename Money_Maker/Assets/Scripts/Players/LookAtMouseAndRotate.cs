27b30b3b975506dd6b75c61c5517b3b160f9a49a
KrSergei <34679066+KrSergei@users.noreply.github.com>
2021-05-21 20:44:57 +0300

    Change scripts
    Изменение скриптов
    Слежение за курсором и стрельба

--- Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
+++ Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
@@ -4,13 +4,24 @@ public class LookAtMouseAndRotate : MonoBehaviour
 {
     public float offset = 5f;   //Угол поворота игрока
 
+    private Vector3 currentMousePos;    //Текущая позиция курсора мышки
+
     void Update()
     {
         //Получение значения курсора экрана в мировых координатах
-        Vector3 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Вычисление угла поворота по оси Z
         float rotateZ = Mathf.Atan2(currentMousePos.y, currentMousePos.x) * Mathf.Rad2Deg;
         //Поворот на заданный угол по оси Z с добавлением укла поворота
         transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
+    }
+
+    /// <summary>
+    /// Возврат текущей позиции мышки
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetMousePos()
+    {
+        return currentMousePos;
     }
 }
