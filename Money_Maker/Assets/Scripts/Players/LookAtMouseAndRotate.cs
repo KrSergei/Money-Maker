2bbe26a73fc573f94f2a25877908b6e12ea0ef29
KrSergei <34679066+KrSergei@users.noreply.github.com>
2021-05-21 20:24:23 +0300

    Add mechanic shooting

--- Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
+++ Money_Maker/Assets/Scripts/Players/LookAtMouseAndRotate.cs
@@ -4,13 +4,24 @@ public class LookAtMouseAndRotate : MonoBehaviour
 {
     public float offset = 5f;   //”гол поворота игрока
 
+    private Vector3 currentMousePos;    //“екущее положение курсора мышки в мировых кординатах
+
     void Update()
     {
         //ѕолучение значени€ курсора экрана в мировых координатах
-        Vector3 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //¬ычисление угла поворота по оси Z
         float rotateZ = Mathf.Atan2(currentMousePos.y, currentMousePos.x) * Mathf.Rad2Deg;
         //ѕоворот на заданный угол по оси Z с добавлением укла поворота
         transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
     }
+
+    /// <summary>
+    /// возврат текущего место положени€ курсора мышки
+    /// </summary>
+    /// <returns></returns>
+    public  Vector3 GetMousePos()
+    {
+        return currentMousePos;
+    }                                                                                                                    
 }
