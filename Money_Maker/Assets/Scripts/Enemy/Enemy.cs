2c2521a76a1a3b048f4e7ac50a4b2b6a944e2460
KrSergei <34679066+KrSergei@users.noreply.github.com>
2021-05-22 01:51:20 +0300

    Add script

---
+++ Money_Maker/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health;  //здоровье
+
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// вычисление урона
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
